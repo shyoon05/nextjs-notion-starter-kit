0681ad29cf90748076abb0bfc1074506855e580d
yoon seung hwan <shyoon05@gmail.com>
2023-01-11 22:40:49 +0900

    Update site.config.ts
    노션 인덱스값 변경

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e4de74b47c2a45f5b0fa70c6ced843d2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
